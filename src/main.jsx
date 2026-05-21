15a9e036500d527890d424883c96e29487f5c717
marcoff484-eng <marcoff484@gmail.com>
2026-05-20 23:13:57 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AlgoTrader from './AlgoTrader'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<AlgoTrader />)
